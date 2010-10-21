d79e92144283712a91033013d92b625ad971b4e9
Zach Holman <github.com@zachholman.com>
2010-10-20 17:00:21 -0700

    needs moar validation!

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,5 +1,7 @@
 class Session < ActiveRecord::Base
 
+  validates_presence_of :facetime_id
+
   def self.available(session)
     @session = Session.find(:first, 
         :conditions => ["id != ? AND last_checked > ?", 
