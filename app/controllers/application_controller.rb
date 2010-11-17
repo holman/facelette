e93315b3f614c575e271bbcf525a7954b0ffc753
Zach Holman <github.com@zachholman.com>
2010-11-17 15:12:01 -0800

    unship it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :done
+
+  def done
+    render :controller => 'sessions', :action => 'done'
+  end
 end
